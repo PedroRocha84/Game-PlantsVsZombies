4b0754b6c67f0ad31cc40a4750da292df92762cd
PedroRocha84 <rocha.pedroandre@outlook.com>
2024-12-04 13:48:15 +0000

    Zombie DP Strategy implemented

--- src/com/learning/zombies/Zombie.java
+++ src/com/learning/zombies/Zombie.java
@@ -1,4 +1,85 @@
 package com.learning.zombies;
 
+import com.codeforall.online.simplegraphics.pictures.Picture;
+
 public class Zombie {
+
+    private String type;
+    private int health;
+    private int maxHealth;
+    private double speed;
+    private int damage;
+    private String picturePath;
+    private Zombie zombie;
+
+    public Zombie(ZombieBuilder builder) {
+        this.type = builder.getType();
+        this.health = builder.getHealth();
+        this.maxHealth = builder.getMaxHealth();
+        this.speed = builder.getSpeed();
+        this.damage = builder.getDamage();
+        this.picturePath = builder.getPicturePath();
+    }
+
+    // Getters
+
+    public String getType() {
+        return type;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public Zombie getZombie() {
+        return zombie;
+    }
+
+
+    // Setters
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+
+    public void setZombie(Zombie zombie) {
+        this.zombie = zombie;
+    }
 }
--- src/com/learning/zombies/ZombieFactory.java
+++ src/com/learning/zombies/ZombieFactory.java
@@ -1,4 +1,35 @@
 package com.learning.zombies;
 
 public class ZombieFactory {
+
+    public static Zombie createZombie(Zombie zombie) {
+        if ("Civil".equals(zombie.getType())) {
+            return createCivilZombie();
+        }
+        return createMilitarZombie();
+    }
+
+    private static Zombie createCivilZombie() {
+        return new ZombieBuilder()
+                .setDamage(10)
+                .setType("Civil")
+                .setHealth(100)
+                .setMaxHealth(200)
+                .setSpeed(0.3)
+                .setPicturePath("./resources/images/zombieCivilS.png")
+                .build();
+    }
+
+    private static Zombie createMilitarZombie() {
+        return new ZombieBuilder()
+                .setDamage(10)
+                .setType("Militar")
+                .setHealth(100)
+                .setMaxHealth(200)
+                .setSpeed(0.2)
+                .setPicturePath("./resources/images/zombieMilitarS.png")
+                .build();
+    }
+
 }
+
