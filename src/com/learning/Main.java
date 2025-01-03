c7a619abebc451d74ff7363bd4354c9252983d24
PedroRocha84 <rocha.pedroandre@outlook.com>
2025-01-03 13:30:31 +0000

    Main class feedback done

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -2,18 +2,26 @@ package com.learning;
 
 import com.learning.play.Menu;
 
-import java.io.FileNotFoundException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-
-
+    public static void main(String[] args) {
         try {
             Menu menu = new Menu();
             menu.init();
             menu.show();
-        }catch (Exception e){
-            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println("File doesn't exist or can't be accessed");
+        } catch (InterruptedException e) {
+            System.err.println("The thread has been interrupted.");
+        } catch (LineUnavailableException e) {
+            System.err.println("Audio channel already in use, or lost!");
+        } catch (UnsupportedAudioFileException e) {
+            System.err.println("Audio file is not supported!");
+        } catch (Exception e) {
+            System.err.println();
         }
     }
 }
